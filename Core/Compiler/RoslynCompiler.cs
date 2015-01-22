cbd04968f2789a20da17a31c5ce21d16befb14dd
h3h3 <h3h3sharp@gmail.com>
2015-01-22 15:18:58 +0100

    Import

---
+++ Core/Compiler/RoslynCompiler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
+
+namespace LeagueSharp.Loader.Core.Compiler
+{
+    public class RoslynCompiler
+    {
+        public RoslynCompiler()
+        {
+            Compile(@"C:\Git\LeagueSharp.Common\LeagueSharp.Common\LeagueSharp.Common\LeagueSharp.Common.csproj");
+        }
+
+        public static async void Compile(string csproj)
+        {
+            var workspace = MSBuildWorkspace.Create();
+            var project = workspace.OpenProjectAsync(csproj).Result;
+
+            var compilation = await project.GetCompilationAsync();
+            var result = compilation.GetDiagnostics();
+
+            Console.WriteLine("References:");
+            foreach (var reference in project.MetadataReferences)
+            {
+                Console.WriteLine(reference.Display);
+            }
+
+            Console.WriteLine("Diagnostics:");
+            foreach (var diagnostic in result.Where(d => d.Severity >= DiagnosticSeverity.Error))
+            {
+                Console.WriteLine(diagnostic);
+            }
+
+            Console.WriteLine("Emit:");
+            if (result.Any(d => d.Severity == DiagnosticSeverity.Error))
+            {
+                Console.WriteLine("Skiped: " + Path.GetFileName(csproj));
+            }
+            else
+            {
+                compilation.Emit("LeagueSharp.Common.dll");
+            }
+        }
+    }
+}
---
+++ ViewModel/ViewModelLocator.cs
@@ -0,0 +1,44 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+using LeagueSharp.Loader.Model;
+
+namespace LeagueSharp.Loader.ViewModel
+{
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// </summary>
+    public class ViewModelLocator
+    {
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            if (ViewModelBase.IsInDesignModeStatic)
+            {
+                SimpleIoc.Default.Register<IDataService, Design.DesignDataService>();
+            }
+            else
+            {
+                SimpleIoc.Default.Register<IDataService, DataService>();
+            }
+
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        /// <summary>
+        /// Gets the Main property.
+        /// </summary>
+        /// <exception cref="ActivationException">if there is are errors resolving
+        ///             the service instance.</exception>
+        public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
+
+        /// <summary>
+        /// Cleans up all the resources.
+        /// </summary>
+        public static void Cleanup()
+        {
+        }
+    }
+}
